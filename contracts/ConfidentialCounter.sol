78e8077e298af84dbd415b7b7924acbeea66c836
Mulya Mewaldi <91192899+Mewaldi@users.noreply.github.com>
2025-10-03 11:01:34 +0700

    Update ConfidentialCounter.sol

--- contracts/ConfidentialCounter.sol
+++ contracts/ConfidentialCounter.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract ConfidentialCounter {
+    uint256 private counter;
+
+    constructor() {
+        counter = 0;
+    }
+
+    function increment(uint256 value) public {
+        counter += value;
+    }
+
+    function getCounter() public view returns (uint256) {
+        return counter;
+    }
+}
 
